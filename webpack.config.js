660bfda38a4c2395384546f49d69436fe6a5f684
Alex <karambasterg@gmail.com>
2021-10-13 14:28:48 +0300

    setup done

--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,7 @@ module.exports = {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
                 generator: {
-                    filename: 'imgs/[hash][ext]'
+                    filename: 'img/[hash][ext]'
                 },
             },
             {
